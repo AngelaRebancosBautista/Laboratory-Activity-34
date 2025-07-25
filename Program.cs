6d0bff59d5a13f50f6852201d429b4ff341d6718
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 13:00:57 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            while (number >= 10) 
+            {
+                    int sum = 0;
+                while (number > 0) 
+                {
+                    sum += number % 10; 
+                    number /= 10; 
+                }
+                   number = sum; 
+            }
+             Console.WriteLine("Single-digit result: " + number);
+        }
+    }
+}
+
+
